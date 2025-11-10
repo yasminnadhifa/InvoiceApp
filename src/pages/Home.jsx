d69c2642a472f90467b260c24f56aa9d6caf4970
Yasmin <yasminnadhifaa@gmail.com>
2025-11-10 12:53:06 +0700

    test husk 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,22 @@
-import React from "react"
-import Navbar from "../components/Navbar"
-import WelcomePage from "../fragments/WelcomePage"
-import Footer from "../components/Footer"
-
+import Navbar from "../components/Navbar";
+import WelcomePage from "../fragments/WelcomePage";
+import Footer from "../components/Footer";
 
 const Home = () => {
-    return (
+  //test aja
+  return (
     <>
-    <Navbar/>
-      <div className='w-full flex flex-col'>
-          <main className='w-full'>
-              <div className='bg-silver py-12'>
-                  <WelcomePage/>
-              </div>
-          </main>
+      <Navbar />
+      <div className="w-full flex flex-col">
+        <main className="w-full">
+          <div className="bg-silver py-12">
+            <WelcomePage />
+          </div>
+        </main>
       </div>
-      <Footer/>
-      </>
-    )
-  }
-  
-  export default Home
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
