bebf092378794473d5bc29ea18ddb333604e76dd
Yasmin <yasminnadhifaa@gmail.com>
2025-11-10 13:29:05 +0700

    test

--- src/pages/Invoices.jsx
+++ src/pages/Invoices.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import Layout from "../components/Layout";
-import Card from "../components/Card";
-import InvoiceForm from "../fragments/InvoiceForm";
-
+import Card from '../components/Card';
+import Layout from '../components/Layout';
+import InvoiceForm from '../fragments/InvoiceForm';
 
 export const Invoices = () => {
-
+  console.log('ddddd ');
   return (
     <>
       <Layout>
-      <div className="space-y-4">
-        <Card>
-        <h1 className="text-2xl font-bold">Add Invoice</h1>
-        </Card>
-      <InvoiceForm></InvoiceForm>
+        <div className="space-y-4">
+          <Card>
+            <h1 className="text-2xl font-bold">Add Invoice</h1>
+          </Card>
+          <InvoiceForm></InvoiceForm>
         </div>
       </Layout>
     </>
