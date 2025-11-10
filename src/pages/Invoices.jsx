fdcfbb69816b281dba921baa658ac0c02194e665
Yasmin <yasminnadhifaa@gmail.com>
2025-11-10 13:36:49 +0700

    test lg

--- src/pages/Invoices.jsx
+++ src/pages/Invoices.jsx
@@ -5,10 +5,11 @@ import InvoiceForm from '../fragments/InvoiceForm';
 
 export const Invoices = () => {
   console.log('ddddd ');
+  console.log('testest');
   return (
     <>
       <Layout>
-        <div></div>
+        <div className="ddsdd"></div>
         <div className="space-y-4">
           <Card>
             <h1 className="text-2xl font-bold">Add Invoice</h1>
